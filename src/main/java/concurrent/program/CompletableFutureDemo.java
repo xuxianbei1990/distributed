1981d922bcc24c9c1126522a51a6ed60b122d4e1
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2020-11-30 10:59:08 +0800

    新增completableFuture demo

--- src/main/java/concurrent/program/CompletableFutureDemo.java
+++ src/main/java/concurrent/program/CompletableFutureDemo.java
@@ -26,7 +26,9 @@ public class CompletableFutureDemo {
             (r) -> {
                 Thread thread = new Thread(r);
                 thread.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+                    System.out.println("thread RuntimeException");
                     throw new RuntimeException(t.getName(), e);
+//                    System.out.println(e);
                 });
                 System.out.println("thread 1");
                 return thread;
@@ -39,7 +41,8 @@ public class CompletableFutureDemo {
         student.setTeacher(new Teacher());
         student.getTeacher().setName("lukaker");
         System.out.println("1");
-        if (student.getAge()  == 1) {
+        if (student.getAge() == 1) {
+            //查下堆栈信息是否会产生死锁 不会
             throw new RuntimeException("sdf");
         }
         return student;
@@ -55,19 +58,80 @@ public class CompletableFutureDemo {
                         System.out.println(student);
                         return student;
                     }, EXECUTOR).get(1000, TimeUnit.MICROSECONDS);
+            EXECUTOR.shutdown();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
-            System.out.println("system" +  getStackTrace(e));
+            System.out.println("system" + getStackTrace(e));
         } catch (TimeoutException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * completableFuture 异常处理
+     */
+    private void testCompletableFutureException() {
+        try {
+            CompletableFuture<Student> completableFuture = CompletableFuture.supplyAsync(() ->
+                    getStudent(), EXECUTOR)
+                    .exceptionally(e -> null);
+            System.out.println(completableFuture.get());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            System.out.println("system" + getStackTrace(e));
+        }
+
+        EXECUTOR.shutdown();
+    }
+
+    /**
+     * Runnable 异常处理
+     */
+    private void testCompletableRunnableException() {
+        try {
+            CompletableFuture completableFuture = new CompletableFuture();
+
+            EXECUTOR.execute(() -> {
+                try {
+                    completableFuture.complete(getStudent());
+                } catch (Exception e) {
+                    completableFuture.completeExceptionally(e);
+                }
+            });
+            System.out.println(completableFuture.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            System.out.println("system" + getStackTrace(e));
+        }
+        EXECUTOR.shutdown();
+    }
+
+    /**
+     * Runnable 异常处理
+     * 说明这里如果抛异常的话，异常会放到future进行返回处理，
+     */
+    private void testFutureException() {
+       Future<Student> studentFuture = EXECUTOR.submit(() ->
+                getStudent());
+        try {
+            System.out.println(studentFuture.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        EXECUTOR.shutdown();
+    }
+
 
     public static void main(String[] args) {
         CompletableFutureDemo demo = new CompletableFutureDemo();
-        demo.test();
+        demo.testFutureException();
     }
 
     public static String getStackTrace(Throwable throwable) {
