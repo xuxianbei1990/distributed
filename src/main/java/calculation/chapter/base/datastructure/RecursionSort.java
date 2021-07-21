929367bf117fe1f574b0f777ccaa87dde3540454
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2021-07-21 13:38:23 +0800

    递归实现，数组排序，非递归实现

--- src/main/java/calculation/chapter/base/datastructure/RecursionSort.java
+++ src/main/java/calculation/chapter/base/datastructure/RecursionSort.java
@@ -1,6 +1,5 @@
 package calculation.chapter.base.datastructure;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -18,7 +17,61 @@ public class RecursionSort {
 
     public static void main(String[] args) {
         RecursionSort recursionSort = new RecursionSort();
-        recursionSort.execute(new int[]{7, 5, 1, 2, 3, 4, 9, 6, 2, 7, 8, 1, 3, 5, 4});
+        int[] seed = new int[]{7, 5, 1, 2, 3, 4, 9, 6, 2, 7, 8, 1, 3, 5, 4};
+        recursionSort.execute(seed);
+        seed = new int[]{7, 5, 1, 2, 3, 4, 9, 6, 2, 7, 8, 1, 3, 5, 4};
+        recursionSort.executeFor(seed);
+    }
+
+    public void executeFor(int[] array) {
+        process1(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private void process1(int[] array) {
+        if (array.length < 2) {
+            return;
+        }
+        int[] temps = new int[array.length];
+        int k = 2;
+
+        while (true) {
+            int kIndex = 0;
+            while (true) {
+                int left = k * kIndex, length = Math.min(k * (kIndex + 1) - 1, array.length - 1);
+                int mid = (left + length) / 2;
+                int right = mid + 1;
+                int tempIndex = 0;
+                while (left <= mid && right <= length) {
+                    if (array[left] < array[right]) {
+                        temps[tempIndex++] = array[left++];
+                    } else {
+                        temps[tempIndex++] = array[right++];
+                    }
+                }
+
+                for (int i = left; i <= mid; i++) {
+                    temps[tempIndex++] = array[i];
+                }
+                for (int i = right; i <= length; i++) {
+                    temps[tempIndex++] = array[i];
+                }
+                tempIndex = 0;
+                for (int i = 0 + k * kIndex; i <= length; i++) {
+                    array[i] = temps[tempIndex++];
+                }
+                kIndex++;
+                if (k * kIndex > array.length) {
+                    break;
+                }
+            }
+
+            k = k << 1;
+            if (k >> 1 > array.length) {
+                break;
+            }
+        }
+
     }
 
     public void execute(int[] array) {
